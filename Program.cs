eee6e0226972d36fbd662172bcece1287c7d4157
Kevin Garabedian <kevin.garabedian@gmail.com>
2023-04-08 00:45:52 -0400

    Working PoC

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Gpt Peer Review Agent!");
+Console.WriteLine(Environment.GetEnvironmentVariable("GITHUB"));
+GitHubPullRequestReviewer agent = new GitHubPullRequestReviewer(Environment.GetEnvironmentVariable("GITHUB"), Environment.GetEnvironmentVariable("CHATGPT"));
+
+await agent.CheckForNewPullRequestsAndPostComments("kevingarabedian/test_repo");
+
+Console.WriteLine("Done... Press any key to exit...");
+Console.ReadKey();
